636d88607851e93eb49c689fb4bf04ab18e99f4c
crab179 <binyang3179@gmail.com>
2019-10-05 23:40:03 +0800

    Initial commit

---
+++ src/main/java/com/qiang/common/utils/phoneVerify/util/SMSUtil.java
@@ -0,0 +1,13 @@
+package com.qiang.common.utils.phoneVerify.util;
+
+/**
+ * @Author Crab
+ * 2019-10-05 23:27
+ **/
+public class SMSUtil {
+    public static final Integer APPID = 14007771;
+    public static final String APPKEY = "";
+    public static final Integer SHORTNOTID = 3878379;
+    public static final Integer UPDPWDID = 3885707;
+    public static final String NOTESIGN = "bin博客";
+}
